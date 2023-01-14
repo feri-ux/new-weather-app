8f53e377890a0f23ee328ab8fb1f8a7b01b67cfe
fereshteh <fereshteh.kalantar@gmail.com>
2023-01-14 17:56:50 +0330

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function showTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = temperature;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "dc738e44d776f0c7801fb0a941f76648";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
